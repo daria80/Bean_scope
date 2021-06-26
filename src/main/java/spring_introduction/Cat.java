6429fe6f89719f7c7df9d44ab18e2bbffbfc6300
smirnova <dddsssmmm555@mail.ru>
2021-06-26 10:29:46 +0300

    add changes

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -2,6 +2,10 @@ package spring_introduction;
 
 public class Cat implements Pet {
 
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Meow-meow");
--- src/main/java/spring_introduction/Test.java
+++ src/main/java/spring_introduction/Test.java
@@ -7,9 +7,15 @@ public class Test {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Person person  = context.getBean("person", Person.class);
 
-        person.callMyPet();
-        System.out.println(person.getSurname() + " " + person.getAge());
+        Cat myCat = context.getBean("cat", Cat.class);
+        Cat yourCat = context.getBean("cat", Cat.class);
+
+        System.out.println("переменные ссылаются на один и тот же объект?\n" + (myCat==yourCat ));
+
+        System.out.println(myCat);
+        System.out.println(yourCat);
+
+        context.close();
     }
 }
