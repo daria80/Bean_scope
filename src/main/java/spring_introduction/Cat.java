05a2c2729bc1a1118542d17a9a02c11cdbc6bd42
smirnova <dddsssmmm555@mail.ru>
2021-06-26 10:41:21 +0300

    add bean singleton

--- src/main/java/spring_introduction/Cat.java
+++ src/main/java/spring_introduction/Cat.java
@@ -2,6 +2,8 @@ package spring_introduction;
 
 public class Cat implements Pet {
 
+    private String name;
+
     public Cat() {
         System.out.println("Cat bean is created");
     }
@@ -9,5 +11,13 @@ public class Cat implements Pet {
     @Override
     public void say() {
         System.out.println("Meow-meow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/spring_introduction/Test.java
+++ src/main/java/spring_introduction/Test.java
@@ -9,12 +9,16 @@ public class Test {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         Cat myCat = context.getBean("cat", Cat.class);
+        myCat.setName("Nicksik");
         Cat yourCat = context.getBean("cat", Cat.class);
+        yourCat.setName("Nicolas");
 
-        System.out.println("переменные ссылаются на один и тот же объект?\n" + (myCat==yourCat ));
+        System.out.println(myCat.getName());
+        System.out.println(yourCat.getName());
 
-        System.out.println(myCat);
-        System.out.println(yourCat);
+//      System.out.println("переменные ссылаются на один и тот же объект?\n" + (myCat==yourCat ));
+//      System.out.println(myCat);
+//      System.out.println(yourCat);
 
         context.close();
     }
